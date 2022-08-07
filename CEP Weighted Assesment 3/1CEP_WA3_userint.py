71d6a75b4678a715e2536024f9f6631f2ed8358c
raamraffles07 <110342879+raamraffles07@users.noreply.github.com>
2022-08-07 12:57:20 +0800

    Add files via upload

---
+++ CEP Weighted Assesment 3/1CEP_WA3_userint.py
@@ -0,0 +1,46 @@
+import sys
+import CEP_WA3_Student
+import cepwa3cca
+
+myCCA = cepwa3cca.cca1()
+
+
+def maincode():
+
+    print("This is RIRC's Record Tracking System!!!")
+
+    print('''
+    Please make choose one of the following 7 choices:
+    (1) Add Student
+    (2) Remove Student
+    (3) Update Student Offence List
+    (4) Update Student's Badge List
+    (5) Update Broken Items
+    (6) Access Student Information 
+    (7) Exit System''')
+
+    inputnum = input("Pick an option: ")
+
+    if inputnum == 1:
+        myCCA.add_student()
+        maincode()
+
+    elif inputnum == 2:
+        myCCA.remove_student()
+        maincode()
+    elif inputnum == 3:
+        myCCA.update_offence_list()
+        maincode()
+    elif inputnum == 4:
+        myCCA.update_badgelist()
+        maincode()
+    elif inputnum == 5:
+        myCCA.update_bi()
+        maincode()
+    elif inputnum == 6:
+        myCCA.access_student_info()
+        maincode()
+    elif inputnum == 7:
+        sys.exit()
+
+maincode()
